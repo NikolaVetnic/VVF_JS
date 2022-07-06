c82218adb213d2b89942c8c3d3bba50626e234eb
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-07-06 08:45:44 +0200

    Another example.

--- js-promises/promise.js
+++ js-promises/promise.js
@@ -1,10 +1,21 @@
-let promise = new Promise((resolve, reject) => {
+const promise0 = new Promise((resolve, reject) => {
     setTimeout(function () {
         Math.random() < 0.5 ? resolve() : reject(new Error("error"));
     }, 250);
 });
 
-promise.then(
+promise0.then(
     (value) => console.log("Success!"),
     (error) => console.log("Error!")
+);
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        Math.random() < 0.5 ? resolve("SUCCESS") : reject(new Error("FAILED"));
+    }, 250);
+});
+
+promise1.then(
+    (result) => console.log(result),
+    (error) => console.log(error)
 );
